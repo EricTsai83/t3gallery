2baa7eb2ae6c5c657949f11ae071f39c8e62c15b
Eric Tsai <eric492718@gmail.com>
2024-05-02 01:41:09 +0800

    Database setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export const metadata = {
 
 function TopNav() {
   return (
-    <nav className="flex w-full items-center justify-between p-4 text-xl font-semibold">
+    <nav className="flex w-full items-center justify-between border-b p-4 text-xl font-semibold">
       <div>Gallery</div>
       <div>Sign In</div>
     </nav>
