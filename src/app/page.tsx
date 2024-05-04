63e362e66468eda5f8e7fee6fd21d9799ea06bb2
Eric Tsai <eric492718@gmail.com>
2024-05-05 02:04:39 +0800

    Use next-image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,23 @@
 import { SignedIn, SignedOut } from "@clerk/nextjs";
 import { db } from "~/server/db";
 import { getMyImages } from "~/server/queries";
+import Image from "next/image";
+
 export const dynamic = "force-dynamic"; // to force dynamic because we want to make sure every time a change is made in our database thsi page's content is updated on the next visit
 async function Images() {
   const images = await getMyImages();
 
   return (
-    <div className="flex flex-wrap gap-4">
+    <div className="flex flex-wrap justify-center gap-4">
       {images.map((image) => (
-        <div key={image.id} className="flex w-48 flex-col">
-          <img src={image.url} alt="" />
+        <div key={image.id} className="flex h-48 w-48  flex-col">
+          <Image
+            src={image.url}
+            style={{ objectFit: "contain" }}
+            width={192}
+            height={192}
+            alt={image.name}
+          />
           <div>{image.name}</div>
         </div>
       ))}
