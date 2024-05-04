6612dc73c0cc7de2064493392f13c556a8eac441
Eric Tsai <eric492718@gmail.com>
2024-05-05 01:49:00 +0800

    Break queries into separate file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
 import { SignedIn, SignedOut } from "@clerk/nextjs";
 import { db } from "~/server/db";
+import { getMyImages } from "~/server/queries";
 export const dynamic = "force-dynamic"; // to force dynamic because we want to make sure every time a change is made in our database thsi page's content is updated on the next visit
 async function Images() {
-  const images = await db.query.images.findMany({
-    orderBy: (model, { desc }) => desc(model.id),
-  });
+  const images = await getMyImages();
 
   return (
     <div className="flex flex-wrap gap-4">
